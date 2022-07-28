cea4d4f2319f6d69d4d7e40504ae0a7d9c3ac8dc
vinayak0127 <vinayakrajbusiness@gmail.com>
2022-07-28 23:35:59 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,31 @@
+let url = "https://www.kommunicate.io/poweredby?utm_source=https://www.kommunicate.io/&utm_medium=webplugin&utm_campaign=poweredby"
+
+function getUrlParameterValue(url, parameter) {
+    let param = url.split('?')[1];
+    let query = new URLSearchParams(param);
+
+    for (let pair of query.entries()) {
+        if (pair[0] === parameter) {
+            console.log(pair[1]);
+        }
+    }
+}
+
+getUrlParameterValue(url, "utm_medium");
+
+/*
+3. Write a javascript function that prints the reverse of a number. Example: if the
+number is 149, then output should be 941
+ */
+
+function reverseNum(n) {
+    var digit, result = 0
+
+    while (n) {
+        digit = n % 10  //  Get right-most digit. Ex. 123/10 → 12.3 → 3
+        result = (result * 10) + digit  //  Ex. 123 → 1230 + 4 → 1234
+        n = n / 10 | 0  //  Remove right-most digit. Ex. 123 → 12.3 → 12
+    }
+
+    return result
+}
